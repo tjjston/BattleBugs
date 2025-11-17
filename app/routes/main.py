60223cf08cc6541ae1b8ac5880711a28f4c643aa
Thomas Johnston <Tjjston@aol.com>
2025-11-17 06:53:50 -0500

    fix: Update latest battles query to use correct timestamp field for accurate data retrieval

--- app/routes/main.py
+++ app/routes/main.py
@@ -6,7 +6,7 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
-    latest_battles = Battle.query.order_by(desc(Battle.timestamp)).limit(5).all()
+    latest_battles = Battle.query.order_by(desc(Battle.battle_date)).limit(5).all()
     upcoming_tournaments = Tournament.query.filter(Tournament.date >= func.current_date()).order_by(Tournament.date).limit(5).all()
     popular_bugs = Bug.query.order_by(desc(Bug.popularity)).limit(5).all()
     return render_template('index.html', battles=latest_battles, tournaments=upcoming_tournaments, bugs=popular_bugs)
